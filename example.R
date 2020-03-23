f94d0b05489d0135bf087a7ecc35ae5fb6a8f661
Andy Quindeau <andyquindeau@Andys-Mac-mini.local>
2020-03-22 20:24:43 -0700

    test

--- example.R
+++ example.R
@@ -5,5 +5,7 @@ source(paste0(p.shiny.fcts, "GUI_regression.R"))
 dt.x <- data.table(x = rnorm(1000), y = rnorm(1000))
 dt.x <- rbind(dt.x, data.table(x = rnorm(1000, 2), y = rnorm(1000, 2)))
 
+
+# test
 GUI_2DClust(dt.x)
 GUI_Regress(dt.x)
