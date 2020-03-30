e4160244ca3584c9c645ce4f8bf7c9023b2053f7
Andy Quindeau <andyquindeau@Andys-Mac-mini.local>
2020-03-29 18:39:51 -0700

    added huber robust linear regression. added functions to generate linear dependent 2D data.

--- example.R
+++ example.R
@@ -1,6 +1,11 @@
 source("libs_functions.R")
+source("dev_functionalities/data_generation.R")
 source(paste0(p.shiny.fcts, "GUI_2DClust.R"))
 source(paste0(p.shiny.fcts, "GUI_regression.R"))
+
+
+# Clustering
+############################################################################################################
 
 # Creating two normally distributed clusters
 dt.x <- data.table(x1 = rnorm(1000), y1 = rnorm(1000))
@@ -10,9 +15,15 @@ dt.x <- rbind(dt.x, data.table(x1 = rnorm(1000, 2), y1 = rnorm(1000, 2)))
 dt.x[, x2 := rnorm(2000)]
 dt.x[, y2 := x2**2]
 
-dt.x %>% ggplot(aes(x2, y2)) + geom_point()
-
 # test
 GUI_2DClust(dt.x)
+
+# Clustering
+############################################################################################################
+
+# Create some data
+dt.x <- GenLinDataNorm(1000, y.norm = c(0, 1), x.norm = c(0, 1), rsq = 0.4)
+
+# Starting the regression
 GUI_Regress(dt.x)
 
--- shiny_modules/module_Regression_linear.R
+++ shiny_modules/module_Regression_linear.R
@@ -51,35 +51,29 @@ LinRegRobPlot <- function(data){
   n.l <- length(y)
   # Building the matrix of independent variable
   X <- matrix(c(rep(1, n.l), x), ncol = 2)
-  # Getting the intercept and coefficient
-  M.coefs <- solve(t(X) %*% X) %*% t(X) %*% y
   
-  # TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO #
-  # TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO #
+  # Computing the weight vecotor for the regression
   w <- HuberLossWins(y, inner.quantile = 0.9, sig.outlier = 5)
   
-  coef.al.hub <- solve(t(X) %*% (w * X)) %*% t(X) %*% (w * y)
-  var.al.hub <- var(x) * coef.al.hub[[2]] ** 2 + var(y * w) - 2 * coef.al.hub[[2]] * cov(x, y * w)
-  se.al.hub <- sqrt(var.al.hub * solve(t(X) %*% X)) %>% diag() %>% .[2]
-  # TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO #
-  # TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO ## TO DO #
-  
+  coef.hub <- solve(t(X) %*% (w * X)) %*% t(X) %*% (w * y)
+  var.hub <- var(x) * coef.hub[[2]] ** 2 + var(y * w) - 2 * coef.hub[[2]] * cov(x, y * w)
+  se.hub <- sqrt(var.hub * solve(t(X) %*% (w * X))) %>% diag() %>% .[2]
   
   # Computing the sums of squares and variance
-  y.hat <- X %*% M.coefs
-  variance <- sum((y - y.hat)**2) / (n.l - 2)
+  y.hat <- X %*% coef.hub
+  variance <- sum(((w * y) - y.hat)**2) / (n.l - 2)
   s.beta <- sqrt(variance / sum((x - mean(x))**2))
   
   if(length(data) > 1000){
     sample_n(data, 1000) %>% ggplot(aes(x, y)) + geom_point() +
-      geom_abline(slope = M.coefs[2, 1], intercept = M.coefs[1, 1], color = "red", size = 2) +
+      geom_abline(slope = coef.hub[2, 1], intercept = coef.hub[1, 1], color = "red", size = 2) +
       theme(legend.position = "none") +
-      ggtitle(paste0("beta = ", signif(M.coefs[2, 1], 3), " se(beta) = ", signif(s.beta, 3)))
+      ggtitle(paste0("beta = ", signif(coef.hub[2, 1], 3), " se(beta) = ", signif(s.beta, 3)))
   } else{
     data %>% ggplot(aes(x, y)) + geom_point() +
-      geom_abline(slope = M.coefs[2, 1], intercept = M.coefs[1, 1], color = "red", size = 2) +
+      geom_abline(slope = coef.hub[2, 1], intercept = coef.hub[1, 1], color = "red", size = 2) +
       theme(legend.position = "none") +
-      ggtitle(paste0("beta = ", signif(M.coefs[2, 1], 3), " se(beta) = ", signif(s.beta, 3)))
+      ggtitle(paste0("beta = ", signif(coef.hub[2, 1], 3), " se(beta) = ", signif(s.beta, 3)))
   }
 }
 
@@ -97,14 +91,18 @@ RegressionLinUI <- function(id, label = "Regression") {
         
         actionButton(ns("do.linreg"), "OLS lin. regression"),
         
+        actionButton(ns("do.roblinreg"), "Huber OLS lin. regression"),
+        
         tags$hr()
         
       ),
       mainPanel(
         
-        plotOutput(ns("plt.just")) , 
+        plotOutput(ns("plt.just")), 
         
-        plotOutput(ns("plt.lin.reg")) 
+        plotOutput(ns("plt.lin.reg")), 
+        
+        plotOutput(ns("plt.hub.lin.reg"))
         
       )
     )
@@ -136,12 +134,26 @@ RegressionLin <- function(input, output, session, data){
     return(plt)
   })
   
+  hublinreg <- eventReactive(input$do.roblinreg, {
+    dt.int <- data()
+    
+    dt.int <- dt.int[(!is.na(x) & !is.na(y))]
+    
+    plt <- LinRegRobPlot(dt.int)
+    
+    return(plt)
+  })
+  
   observe({
     output$plt.just <- renderPlot({plotting()})
   })
   
   observe({
     output$plt.lin.reg <- renderPlot({linreg()})
+  })
+  
+  observe({
+    output$plt.hub.lin.reg <- renderPlot({hublinreg()})
   })
   
 }
